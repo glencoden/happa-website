f7355e44c16d1dda81fd2adcb02b492d794c63d1
glencoden <80849247+glencoden@users.noreply.github.com>
2023-08-24 11:47:40 +0000

    Build: (34c5c6c) add article to request service, finish lunch feature

---
+++ _astro/ArticleDefault.1cb95346.js
@@ -0,0 +1,1 @@
+import{A as d}from"./ArticleDefault.6f728709.js";import"./web.eda076f5.js";import"./solid.280e3150.js";import"./Button.a9681659.js";import"./Image.e9f895ec.js";import"./Link.9abb73dd.js";import"./RichText.9642ab6b.js";import"./Text.d84ae04f.js";export{d as default};
--- _astro/Text.3024c3ba.js
+++
@@ -1,1 +0,0 @@
-import{T as a}from"./Text.d5e5dab6.js";import"./solid.3579ee0f.js";export{a as default};
