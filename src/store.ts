031a84360cda838cc9552674a00c687f7e923e7b
glen coden <info@simonmeyer.de>
2023-10-16 15:05:31 +0200

    add persistent locale

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import { atom } from 'nanostores'
+import { persistentAtom } from '@nanostores/persistent'
 import { Locale } from './enums/Locale'
 
-export const locale = atom(Locale.German)
+export const locale = persistentAtom<Locale>('locale', Locale.German)
 
 export const isMenuOpen = atom(false)
