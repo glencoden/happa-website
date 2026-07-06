e92d1274920042919062037165256b8c9a2603ad
Glen Coden <info@simonmeyer.de>
2026-07-06 11:27:00 +0200

    feat: bake lunch page

--- src/features/Lunch/Lunch.tsx
+++ src/features/Lunch/Lunch.tsx
@@ -1,45 +1,29 @@
-import { createSignal, onMount, Show } from 'solid-js'
 import type { Component } from 'solid-js'
 import ArticleDefault from '../../components/ArticleDefault/ArticleDefault'
 import Menu from '../../components/Menu/Menu'
-import { requestService } from '../../services/requestService'
-import type { Article } from '../../types/Article'
+import type { BakedArticle } from '../../types/BakedArticle'
 import type { MenuType } from '../../types/MenuType'
 
-const Lunch: Component = () => {
-    const [menu, setMenu] = createSignal<MenuType | null>(null)
-    const [description, setDescription] = createSignal<Article | null>(null)
+type Props = {
+    menu: MenuType
+    description: BakedArticle
+}
 
-    onMount(async () => {
-        try {
-            const lunchMenu = await requestService.getLunchMenuData()
-            setMenu(lunchMenu)
-
-            const lunchDescription = await requestService.getLunchDescriptionData()
-            setDescription(lunchDescription)
-        } catch (error) {
-            console.error(error)
-        }
-    })
-
+const Lunch: Component<Props> = (props) => {
     return (
         <>
-            <Show when={menu() !== null}>
-                <Menu
-                    title={menu()!.title}
-                    menu={menu()!.menu}
-                    buttonText={null}
-                    buttonLink={null}
-                />
-            </Show>
+            <Menu
+                title={props.menu.title}
+                menu={props.menu.menu}
+                buttonText={null}
+                buttonLink={null}
+            />
 
-            <Show when={description() !== null}>
-                <ArticleDefault
-                    imageUrl={description()!.imageUrl}
-                    title={description()!.title}
-                    content={description()!.content}
-                />
-            </Show>
+            <ArticleDefault
+                image={props.description.image}
+                title={props.description.title}
+                content={props.description.content}
+            />
         </>
     )
 }
