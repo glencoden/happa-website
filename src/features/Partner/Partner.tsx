7d2fde060971e902286a7d59a8b42c1a3728d0d1
Glen Coden <info@simonmeyer.de>
2026-07-06 11:33:25 +0200

    feat: bake partner page

--- src/features/Partner/Partner.tsx
+++ src/features/Partner/Partner.tsx
@@ -1,59 +1,44 @@
-import { createSignal, For, onMount, Show } from 'solid-js'
+import { For, Show } from 'solid-js'
 import type { Component } from 'solid-js'
 import ArticleBanner from '../../components/ArticleBanner/ArticleBanner'
 import ArticleCard from '../../components/ArticleCard/ArticleCard'
 import Carousel from '../../components/Carousel/Carousel'
-import { requestService } from '../../services/requestService'
-import type { Article } from '../../types/Article'
+import type { BakedArticle } from '../../types/BakedArticle'
 import styles from './Partner.module.css'
 
-const Partner: Component = () => {
-    const [banner, setBanner] = createSignal<Article | null>(null)
-    const [partners, setPartners] = createSignal<(Article | null)[] | null>(null)
+type Props = {
+    banner: BakedArticle
+    partners: BakedArticle[]
+}
 
-    onMount(async () => {
-        try {
-            const banner = await requestService.getPartnerBannerData()
-            setBanner(banner)
-
-            const partners = await requestService.getPartnersData()
-            setPartners(partners)
-        } catch (error) {
-            console.error(error)
-        }
-    })
-
+const Partner: Component<Props> = (props) => {
     return (
         <>
-            <Show when={banner() !== null && banner()!.subtitle !== null}>
+            <Show when={props.banner.subtitle !== null}>
                 <ArticleBanner
-                    title={banner()!.title}
-                    subtitle={banner()!.subtitle!}
-                    content={banner()!.content}
-                    linkText={banner()!.buttonText!}
-                    linkUrl={banner()!.buttonLink!}
-                    imageUrl={banner()!.imageUrl}
+                    title={props.banner.title}
+                    subtitle={props.banner.subtitle!}
+                    content={props.banner.content}
+                    linkText={props.banner.buttonText!}
+                    linkUrl={props.banner.buttonLink!}
+                    image={props.banner.image}
                 />
             </Show>
 
-            <Show when={partners() !== null}>
-                <Carousel>
-                    <For each={partners()!}>
-                        {(partner) => (
-                            <Show when={partner !== null}>
-                                <div class={styles.carouselItemBox}>
-                                    <ArticleCard
-                                        title={partner!.title}
-                                        content={partner!.content}
-                                        imageUrl={partner!.imageUrl}
-                                        linkUrl={partner!.buttonLink!}
-                                    />
-                                </div>
-                            </Show>
-                        )}
-                    </For>
-                </Carousel>
-            </Show>
+            <Carousel>
+                <For each={props.partners}>
+                    {(partner) => (
+                        <div class={styles.carouselItemBox}>
+                            <ArticleCard
+                                title={partner.title}
+                                content={partner.content}
+                                image={partner.image}
+                                linkUrl={partner.buttonLink!}
+                            />
+                        </div>
+                    )}
+                </For>
+            </Carousel>
         </>
     )
 }
