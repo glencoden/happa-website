da68109e189747dc9a81a0c368a8aa0d6821735e
Glen Coden <info@simonmeyer.de>
2026-07-06 11:07:38 +0200

    chore: migrate to pnpm and upgrade astro to v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
-
 import solidJs from '@astrojs/solid-js'
 
-// https://astro.build/config
 export default defineConfig({
-    integrations: [ solidJs() ],
+    site: 'https://happa-berlin.com',
+    integrations: [solidJs()],
+    image: {
+        domains: ['cdn.sanity.io'],
+    },
 })
